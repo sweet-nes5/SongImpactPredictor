c82253f86924d79ee3a9528775f88dd9df9eb601
dahmani <sonia.dahmani@etu.univ-paris-diderot.fr>
2023-12-10 14:00:40 +0100

    Cnormalisation des donn

--- main_aux.py
+++ main_aux.py
@@ -1,13 +1,20 @@
 
 from collections import defaultdict
+from sklearn.preprocessing import MinMaxScaler 
 
 
+
+def extract_and_normalize_features(list_parts):
+	#using the MinMaxScaler from sklearn.preprocessing, it takes a 2D tab so we reshape list_parts into a 2D tab 
+	list_2d = [[feature] for feature in list_parts]
+	scaler = MinMaxScaler()
+	normalized_features = scaler.fit_transform(list_2d) #fit to data, then transform it 
+	#we re reshape it again into a list
+	normalized_features_list = [features[0] for features in normalized_features]
+	return normalized_features_list
 
 """ Parameters: filename (str)
 	returns: songs_dict (defaultdict, key= song title, value = features), score(list) """
-
-
-
 def read_dataset(filename):
 	popularity_scores = []  # song_popularity (what we are seeking to predict), value between 0 and 100 for now ( TODO NORMALIZE THE VALUE INTO A [0,1] VALUE)
 
@@ -26,16 +33,16 @@ def read_dataset(filename):
 				#don't add it if it's already in songs_dict
 				if song_name not in songs_dict:
 					if len(line_parts) == len(header_parts): #check that the data in each line aligns with column name
-						score = int(line_parts[1]) # storing the song's popularity rate in score (TODO NORMALIZE)
-						features = [float(i) for i in line_parts[1:]]
-						songs_dict[song_name] = features
-						popularity_scores.append(score)
+						features_list = extract_and_normalize_features(line_parts[1:])
+						#print(features_list)
+						score = features_list[0]
+						songs_dict[song_name] = features_list[1:]
+						popularity_scores.append(score)					
 				else:
 					continue
 
-
-			print(songs_dict)
-
+			#print(songs_dict)
+			#print(popularity_scores)
 			return songs_dict, popularity_scores
 
 	except FileNotFoundError:
