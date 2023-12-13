2bd97f58b4860ecbe2a96a62ca74178c0e25ed3d
dahmani <sonia.dahmani@etu.univ-paris-diderot.fr>
2023-12-13 15:02:54 +0100

    fonction split lines

--- main_aux.py
+++ main_aux.py
@@ -1,7 +1,21 @@
-
+import random
 from collections import defaultdict
 from sklearn.preprocessing import MinMaxScaler 
 
+
+
+
+
+""" extracts the features associated to each song title and normalizes them using module sklearn_preprocessing 
+in order to tranform the data into [0,1] values so it will be easier to calculate and make the results more balanced
+
+
+	Args: 
+		list_parts: a list of floats (list[float])
+	returns: 
+		normalized_features_list: list containing the values of each feature but fitted to data 
+
+ """
 def extract_and_normalize_features(list_parts):
 	#using the MinMaxScaler from sklearn.preprocessing, it takes a 2D tab so we reshape list_parts into a 2D tab 
 	list_2d = [[feature] for feature in list_parts]
@@ -11,8 +25,14 @@ def extract_and_normalize_features(list_parts):
 	normalized_features_list = [features[0] for features in normalized_features]
 	return normalized_features_list
 
-""" Parameters: filename (str)
-	returns: songs_dict (defaultdict, key= song title, value = features), score(list) """
+
+
+
+""" Reads data from input file and makes it readable for the model by creating a dictionary of songs with song_title as a key and a list of floats representing features as values
+	Args: filename (str)
+	returns: songs_dict (defaultdict, key= song title, value = features), score(list)
+
+	 """
 def read_dataset(filename):
 	popularity_scores = []  # song_popularity (what we are seeking to predict), value between 0 and 100 for now ( TODO NORMALIZE THE VALUE INTO A [0,1] VALUE)
 
@@ -21,6 +41,9 @@ def read_dataset(filename):
 
 	try:
 		with open(filename, 'r') as input_file: 
+			if not input_file.readline():
+				return  #incase the file is empty
+
 			header = input_file.readline() #extract the header
 			header_parts = [header.strip() for part in header.strip().split(',')]  #  ['song_name', 'song_popularity', 'song_duration_ms', 'acousticness', 'danceability', 'energy', 'instrumentalness', 'key', 'liveness', 'loudness', 'audio_mode', 'speechiness', 'tempo', 'time_signature', 'audio_valence']
 
@@ -47,5 +70,58 @@ def read_dataset(filename):
 		print("File does not exist: {filename}")
 
 
+
+""" Separates the data set into two sub-sets: training_set and test_set. Pseudo-randomly
+	Args: 
+		input: string, name of the input file containg our dataset (file)
+		seed: integer, the seed of the pseudo-random generator uses. using the same seed and same input generates the same outputs (but do we want that ?), and using different seeds generates different results 
+		output1: string, name of the first output file 
+		output2: string, name of the second output file 
+		ratio: float between 0 and 1, to get the size of the output1 file (for example ratio= 0.65 if we want a ratio of 65% training and 35% test)
+
+ """
+def split_lines(input, seed, output1, output2, ratio): 
+	try: 
+		with open(input, 'r') as input_file: 
+			random.seed(seed)
+			lines = open(input, 'r').readlines()
+			if not lines:
+				return #in case the file is empty
+
+			#counting the size of each file with the given ratio: 
+			input_size= len(lines)
+			output1_capacity = int(input_size * ratio)  #Training set size, which makes the size of the test size as: input_size - training_set_size 			
+			output2_capacity = input_size - output1_capacity
+			random.shuffle(lines) #shuffles the lines randomly 
+			header = "song_title, song_popularity, song_duration, accousticness, danceability, energy, instrumentalness, key, liveness, loudness\n"
+			with open(output1, 'w') as out1, open(output2, 'w') as out2:
+				out1.write(header)
+				out2.write(header)
+				for line in lines:
+					if random.randint(0, 1) and output1_capacity > 0:
+							out1.write(line)
+							output1_capacity -= 1 #at each line written on this file, we decrease its capacity 
+					elif output2_capacity > 0:
+							out2.write(line)
+							output2_capacity -= 1
+					else:
+						out1.write(line)
+						output1_capacity -= 1
+
+				
+
+		#opening the output files in read mode to print the number of lines (TO DELETE LATER)
+		#with open(output1, 'r') as out1, open(output2, 'r') as out2:
+		#	print(len(out1.readlines()))
+		#	print(len(out2.readlines()))
+	except FileNotFoundError: 
+		print("File does not exist : {input}")
+
+
+
+
+"""we chose to use csv files for our data becasuse it is tabular with rows and columns representing different features """
+
 if __name__ == "__main__":
-	songs_dict, socres= read_dataset('song_data.csv')
+	#songs_dict, scores= read_dataset('song_data.csv')
+	split_lines('song_data.csv', 56, 'train.csv', 'test.csv', 0.65)
